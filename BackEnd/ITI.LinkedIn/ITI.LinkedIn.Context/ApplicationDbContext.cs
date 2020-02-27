d107716e8953e4c7b11df8c9cac1b10ab7b339ec
Hossam Ali <hossameldinali96@gmail.com>
2020-02-27 15:36:55 +0200

    Added EmploymentType Entity & Manager

--- BackEnd/ITI.LinkedIn/ITI.LinkedIn.Context/ApplicationDbContext.cs
+++ BackEnd/ITI.LinkedIn/ITI.LinkedIn.Context/ApplicationDbContext.cs
@@ -19,6 +19,7 @@ namespace ITI.LinkedIn.Context
         // To add a new Model
         public virtual DbSet<Skill> Skills { get; set; }
         public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<EmploymentType> EmploymentTypes { get; set; }
 
         public static ApplicationDbContext Create()
         {
---
+++ BackEnd/ITI.LinkedIn/ITI.LinkedIn.Core/SingletonManagers/SingletonEmploymentTypeManager.cs
@@ -0,0 +1,29 @@
+﻿using ITI.LinkedIn.Context;
+using ITI.LinkedIn.Models;
+using ITI.LinkedIn.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.LinkedIn.Core.SingletonManagers
+{
+    public class SingletonEmploymentTypeManager : Repository<EmploymentType, ApplicationDbContext>
+    {
+        static SingletonEmploymentTypeManager manager;
+
+        private SingletonEmploymentTypeManager(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public static SingletonEmploymentTypeManager GetInstance(ApplicationDbContext context)
+        {
+            if (manager == null)
+            {
+                manager = new SingletonEmploymentTypeManager(context);
+            }
+            return manager;
+        }
+    }
+}
