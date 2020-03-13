a475906224da50d99d9bf6eb35405d7f5ef40990
Hossam Ali <hossameldinali96@gmail.com>
2020-03-13 10:35:43 +0200

    Added Photos Custom Validators

---
+++ BackEnd/ITI.LinkedIn/ITI.LinkedIn.Models/CustomValidators/ValidatePhotosTypeAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ITI.LinkedIn.Models.CustomValidators
+{
+    public class ValidatePhotosTypeAttribute : ValidationAttribute
+    {
+        /*protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            List<HttpPostedFileBase> files = value as List<HttpPostedFileBase>;
+            string defaultErrorMessage = "Only Images Allowed";
+
+            foreach (var file in files)
+            {
+                if (file != null)
+                {
+                    if (!Regex.IsMatch(file.FileName, @"([a-zA-Z0-9\s_\\.\-:])+(.png|.jpg|.gif)$", RegexOptions.IgnoreCase))
+                    {
+                        return new ValidationResult(ErrorMessage ?? defaultErrorMessage);
+                    }
+                }
+            }
+            return ValidationResult.Success;
+        }*/
+    }
+}
